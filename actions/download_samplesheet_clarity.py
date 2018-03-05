2a0684b62e3f946419b8813be0b25c1893a68906
PerLundmark <per.lundmark@medsci.uu.se>
2018-03-05 10:42:37 +0100

    Added samplesheet fallback action, more additions to clarity download

--- actions/download_samplesheet_clarity.py
+++ actions/download_samplesheet_clarity.py
@@ -6,6 +6,11 @@ from genologics.config import BASEURI, USERNAME, PASSWORD
 
 import os
 from tempfile import mkdtemp
+import zipfile
+import re
+
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
 class DownloadSamplesheetClarity(Action):
     """
@@ -35,19 +40,41 @@ class DownloadSamplesheetClarity(Action):
         containers = lims.get_containers(name=flowcell_name)
         processes = list(self.get_processes(containers))
         newest_process_id = self.highest_id(processes)
-        context = ExtensionContext.create(newest_process_id)
         cwd = os.getcwd()
+        #generate a fresh temp dir to mimic lims behaviour (avoid overwriting samplesheets)
+        temp_wd = mkdtemp()
+        os.chdir(temp_wd)
+        context = ExtensionContext.create(newest_process_id)
         try:
-            #generate a fresh temp directory to mimic LIMS behaviour (avoid overwriting samplesheets)
-            temp_wd = mkdtemp()
-            os.chdir(temp_wd)
             samplesheet_file = context.local_shared_file("Sample Sheet")
-            samplesheet = samplesheet_file.read()
-            os.chdir(cwd)
+            # samplesheet file can be both single file and zip file with names like 92-102630_HH7LMCCXY_samplesheet.csv in archive.
+            if zipfile.is_zipfile(samplesheet_file):
+                #extract appropriate zip member
+                print "Is a zip"
+                zf = zipfile.ZipFile(samplesheet_file, 'r')
+                zipped_files = zf.namelist()
+                flowcell_pattern = re.compile('_' + flowcell_name + '_samplesheet')
+                matching_files = []
+                for filename in zipped_files:
+                    if flowcell_pattern.search(filename):
+                        matching_files.append(filename)
+                if len(matching_files) > 1:
+                    raise LookupError('More than one samplesheet matching flowcell name in zipfile!')
+                elif len(matching_files) == 0:
+                    raise LookupError('No matching samplesheet in zipfile')
+                else:
+                    samplesheet = zf.read(matching_files[0])
+
+            else:
+                print "Nope, not a zip"
+                samplesheet = samplesheet_file.read()
 
         except IOError as err:
             self.logger.error('IOError while attempting to read samplesheet: {}'.format(err.message))
             raise err
+        except LookupError as err:
+            self.logger.error('LookupError while attempting to read samplesheet in zip: {}'.format(err.message))
+            raise err
 
+        os.chdir(cwd)
         return (True, samplesheet)
-
