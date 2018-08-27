2835b560ea00217e984c4af6fef48aa535f4e95f
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2018-08-27 16:11:37 +0200

    Fix Slack tests

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -12,7 +12,7 @@ class TestSlackPoster(BaseActionTestCase):
     def test_successful_post(self, notifier_mock):
         action = self.get_action_instance()
         action.config["slack_webhook_url"] = "this-is-the-webhook-url"
-        self.assertIsNone(action.run("channel", "user", "message"))
+        self.assertIsTrue(action.run("channel", "user", "message"))
 
     @mock.patch("lib.slack.SlackNotifier", autospec=True)
     def test_unsuccessful_post(self, notifier_mock):
@@ -20,5 +20,4 @@ class TestSlackPoster(BaseActionTestCase):
         action.config["slack_webhook_url"] = "this-is-the-webhook-url"
         notifier = notifier_mock.return_value
         notifier.post_message.side_effect = requests.exceptions.HTTPError("a mock HTTPError")
-        with self.assertRaises(requests.exceptions.HTTPError):
-            action.run("channel", "user", "message")
+        self.assertIsFalse(action.run("channel", "user", "message"))
