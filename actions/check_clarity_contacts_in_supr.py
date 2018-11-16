fec10e376552790aa97da62467b013d9b3d0b666
PerLundmark <per.lundmark@medsci.uu.se>
2018-11-16 14:42:42 +0100

    Action and workflow for SUPR contact check

--- actions/check_clarity_contacts_in_supr.py
+++ actions/check_clarity_contacts_in_supr.py
@@ -6,6 +6,10 @@ from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 from lib.supr_utils import * 
 
+#disable ssl warnings
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
+
 
 class CheckClarityContactsInSupr(Action):
     """
@@ -13,6 +17,7 @@ class CheckClarityContactsInSupr(Action):
     SUPR accounts by email.
     Then composes an email body to notify project coordinators.
     """
+
     def fetch_open_projects(self):
         lims = Lims(BASEURI, USERNAME, PASSWORD)
         lims.check_version()
@@ -21,31 +26,73 @@ class CheckClarityContactsInSupr(Action):
 	for project in projects:
 		if project.open_date and not project.close_date:
 			filtered_projects.append(project)
-			
         return filtered_projects
 
-    def check_email_in_supr(email_adress):
-    	supr_utils.search_by_email(base_url, email_adress, user, key)
+    def check_email_in_supr(self, email_adress):
+        email_missing = False
+        email_multi = False
 
+        try:
+            pi_id = SuprUtils.search_by_email(self.supr_base_url, email_adress, self.supr_user, self.supr_key)
+        except AssertionError as ae:
+            if ("no hits" in ae.message):
+                email_missing = True
+            elif ("more than one" in ae.message):
+                email_multi = True
 
-    def run(self): #supr_base_api_url, api_user, api_key
-        #fetch projects
+        return (email_missing, email_multi)
+
+    def run(self): #supr_base_url, supr_user, supr_key
+        #TODO:debug vars, remove
+	supr_base_url = "https://supr.snic.se/api"
+	supr_user = "api-2"
+	supr_key= "Acgxd6Hvns"
+
+        self.supr_base_url = supr_base_url
+        self.supr_user = supr_user
+        self.supr_key = supr_key
+
         projects = self.fetch_open_projects()
+	email_body = ""
+        debug_break = 30 #TODO: remove
 	for project in projects:
-		pi_email = project.udf.get("Email of PI")
-		bio_email = project.udf.get("Email of bioinformatics responsible person")
-		#primary_email = project.udf.get("Email of primary contact")
+            pi_missing = False
+            pi_multi = False
+            bio_missing = False
+            bio_multi = False
 
-		if (pi_email):
-			check_email_in_supr(pi_email)
+            debug_break =- 1        #TODO: remove
+            if (debug_break == 0):
+                break
+
+            pi_email = project.udf.get("Email of PI")
+            bio_email = project.udf.get("Email of bioinformatics responsible person")
+            #primary_email = project.udf.get("Email of primary contact")
+            if (pi_email):
+                (pi_missing, pi_multi) = self.check_email_in_supr(pi_email) 
+  
+            if (bio_email):
+                (bio_missing, bio_multi) = self.check_email_in_supr(bio_email)
+ 
+            if (pi_missing or pi_multi or bio_missing or bio_multi):
+                pi_name = project.udf.get("Name of PI")
+                if (not pi_name):
+                    pi_name = "Name missing from LIMS"
+                bio_name = project.udf.get("Name of bioinformatics responsible person")
+                if (not bio_name):
+                    bio_name = "Name missing from LIMS"
+                email_body += "<hr>Project: " + project.name + ": <br><br>"
+
+                if (pi_missing):
+                    email_body += "PI missing from SUPR ( " + pi_name + ", " + pi_email + " )<br>"
+                if (pi_multi):
+                    email_body += "PI has multiple accounts in SUPR ( " + pi_name + ", " + pi_email + " )<br>"
+                if (bio_missing):
+                    email_body += "BIO missing from SUPR ( " + bio_name + ", " + bio_email + " )<br>"
+                if (bio_multi):
+                    email_body += "BIO has multiple accounts in SUPR ( " + bio_name + ", " + bio_email + " )<br>"
+
+        return (True, email_body)
 
 
 
-
-		print "---------------------"
-		print project.open_date
-		print project.close_date
-		print "---------------------"
-        #Retrieve contacs
-
-        #Check SUPR
