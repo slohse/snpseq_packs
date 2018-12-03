1049bb730f31823bd8d9b3b3b99fa148b6118647
b97pla <pontus.larsson@medsci.uu.se>
2018-12-03 17:31:26 +0100

    moved logger setup, made code more explicit

--- actions/lib/purge_remote_folder.py
+++ actions/lib/purge_remote_folder.py
@@ -11,18 +11,45 @@ import socket
 
 class PurgeRemoteFolders(object):
 
-    def __init__(self, biotank_archive_folder, age_in_days, dryrun):
+    def __init__(self, biotank_archive_folder, age_in_days, dryrun, execution_id):
+        # do some explicit sanity checking here
+        if not biotank_archive_folder:
+            raise ValueError(
+                "an illegal value for biotank_archive_folder was specified: {val}".format(val=biotank_archive_folder))
+        if int(age_in_days) <= 0:
+            raise ValueError(
+                "an illegal value for age_in_days was specified: {val}".format(val=age_in_days))
+
         self.biotank_archive_folder = str(biotank_archive_folder)
         self.age_in_days = int(age_in_days)
         self.dryrun = bool(dryrun)
 
+        # the directory to look for things to clean are constructed from e.g. the hostname of the machine
+        # as a precaution, this is hardcoded here and can not be supplied as a parameter
         hostname = socket.gethostname()
-        self.directory = self.sanitize_path(
+        self.archive_base_dir = self.sanitize_path(
             self.biotank_archive_folder,
             os.path.join(
                 "/data",
                 hostname))
-        self.logfile = os.path.join(self.directory, "KEEP_removed_archives.log")
+        self.log = self.setup_logger(execution_id)
+
+    def setup_logger(self, execution_id):
+
+        # setup a formatter that will include the st2 execution id with the log
+        formatter = logging.Formatter('%(message)s,%(asctime)s,{id}'.format(id=execution_id))
+
+        # the logfile will be written in the folder where the cleanup is taking place, the "KEEP" prefix will keep it
+        # from being removed
+        logfile = os.path.join(self.archive_base_dir, "KEEP_removed_archives.log")
+        ch = logging.FileHandler(logfile, delay=True)
+        ch.setFormatter(formatter)
+
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(ch)
+
+        return logger
 
     @staticmethod
     def sanitize_path(dirty_path, directory):
@@ -41,46 +68,43 @@ class PurgeRemoteFolders(object):
         return clean_path
 
     def get_files_and_folders(self):
-        older_than = datetime.date.today() - datetime.timedelta(days=self.age_in_days)
+        remove_if_modified_before_this = datetime.date.today() - datetime.timedelta(days=self.age_in_days)
 
         def _path_should_be_purged(p):
-            return datetime.date.fromtimestamp(
-                os.stat(
-                    os.path.join(
-                        self.directory, p)).st_mtime) < older_than and not p.startswith("KEEP")
+            path_modtimestamp = os.stat(os.path.join(self.archive_base_dir, p)).st_mtime
+            path_modification_date = datetime.date.fromtimestamp(path_modtimestamp)
+            return path_modification_date < remove_if_modified_before_this and not p.startswith("KEEP")
 
         return filter(
             _path_should_be_purged,
-            os.listdir(self.directory))
+            os.listdir(self.archive_base_dir))
 
-    def purge_files_and_folders(self, files_and_folders_to_purge, logger):
+    def purge_files_and_folders(self, files_and_folders_to_purge):
 
-        def _purge_path(p):
-            purge_fn = shutil.rmtree if os.path.isdir(p) else os.unlink
+        for file_or_folder_to_purge in files_and_folders_to_purge:
+            sanitized_path = self.sanitize_path(file_or_folder_to_purge, self.archive_base_dir)
+            # use shutil.rmtree for directories and os.unlink for everything else
+            purge_fn = shutil.rmtree if os.path.isdir(sanitized_path) else os.unlink
             if not self.dryrun:
-                purge_fn(p)
-                logger.info(p)
+                purge_fn(sanitized_path)
+                self.log.info(sanitized_path)
 
-        map(
-            _purge_path,
-            map(
-                lambda p: self.sanitize_path(p, self.directory),
-                files_and_folders_to_purge))
-
-    def purge(self, logger):
+    def purge(self):
 
         try:
             files_and_folders_to_purge = self.get_files_and_folders()
         except Exception as e:
-            print(
-                "encountered the following error when trying to determine files and folders to purge: {msg}".format(msg=e))
+            self.log.error(
+                "encountered the following error when trying to determine files and folders to purge: {msg}".format(
+                    msg=e))
             raise
 
         try:
-            self.purge_files_and_folders(files_and_folders_to_purge, logger)
+            self.purge_files_and_folders(files_and_folders_to_purge)
         except Exception as e:
-            print(
-                "encountered the following error when trying to purge files and folders: {msg}".format(msg=e))
+            self.log.error(
+                "encountered the following error when trying to purge files and folders: {msg}".format(
+                    msg=e))
             raise
 
         return files_and_folders_to_purge
@@ -108,18 +132,10 @@ def main():
     parser.set_defaults(dryrun=False)
     args = parser.parse_args()
 
-    cleaner = PurgeRemoteFolders(args.biotank_archive_folder, args.age_in_days, args.dryrun)
-
     execution_id = os.environ.get("ST2_ACTION_EXECUTION_ID", "")
-    # create logger
-    logger = logging.getLogger("purge_remote_folder")
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(message)s,%(asctime)s,{id}'.format(id=execution_id))
-    ch = logging.FileHandler(cleaner.logfile, delay=True)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    cleaner = PurgeRemoteFolders(args.biotank_archive_folder, args.age_in_days, args.dryrun, execution_id)
 
-    result_as_json = cleaner.purge(logger)
+    result_as_json = cleaner.purge()
     print(json.dumps(result_as_json))
 
 
